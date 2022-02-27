85a6095b0ffde5f3ff4829f985f9147ec824eb0b
mercklyzer <mercklyzer@gmail.com>
2022-02-27 22:24:55 +0800

    Tailwind setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="h-4 w-4 bg-red-700"></div>
+    </div>
+  );
+}
+
+export default App;

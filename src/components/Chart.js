d88bc26b3168cbb8703c0d1ea9ed93d0ab6fb160
mercklyzer <mercklyzer@gmail.com>
2022-02-27 23:31:35 +0800

    install recharts

---
+++ src/components/Chart.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Chart = props => {
+    return (
+        <div className="p-4 shadow-lg">
+            <div className="font-bold text-lg">Chart</div>
+            
+        </div>
+    )
+}
+
+export default Chart

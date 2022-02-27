a30a88628648d04e3068bb501ec8008d07d2e4ad
mercklyzer <mercklyzer@gmail.com>
2022-02-27 23:40:40 +0800

    Add base chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,25 @@
 import React from "react"
+import {
+    LineChart,
+    Line,
+    XAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+  } from "recharts";
 
-const Chart = props => {
+const Chart = ({ title, data, dataKey, grid }) => {
     return (
-        <div className="p-4 shadow-lg">
-            <div className="font-bold text-lg">Chart</div>
-            
+        <div className="p-4 shadow-lg m-4 rounded-lg">
+            <div className="font-bold text-lg">{title}</div>
+            <ResponsiveContainer width="100%" aspect={4 / 1}>
+                <LineChart data={data}>
+                <XAxis dataKey="name" stroke="#5550bd" />
+                <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+                <Tooltip />
+                {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+                </LineChart>
+            </ResponsiveContainer>
         </div>
     )
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,3 +1,57 @@
+import React from "react"
+import Chart from "./Chart";
+
+const userData = [
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "Mar",
+      "Active User": 5000,
+    },
+    {
+      name: "Apr",
+      "Active User": 4000,
+    },
+    {
+      name: "May",
+      "Active User": 3000,
+    },
+    {
+      name: "Jun",
+      "Active User": 2000,
+    },
+    {
+      name: "Jul",
+      "Active User": 4000,
+    },
+    {
+      name: "Agu",
+      "Active User": 3000,
+    },
+    {
+      name: "Sep",
+      "Active User": 4000,
+    },
+    {
+      name: "Oct",
+      "Active User": 1000,
+    },
+    {
+      name: "Nov",
+      "Active User": 4000,
+    },
+    {
+      name: "Dec",
+      "Active User": 3000,
+    },
+];
+
 const Content = props => {
     return (
         <div className={`${props.className} `}>
@@ -6,7 +60,12 @@ const Content = props => {
             </div>
 
             <div className="h-[100vh]">
-
+                <Chart
+                    data={userData}
+                    title="User Analytics"
+                    grid
+                    dataKey="Active User"
+                />
             </div>
 
 

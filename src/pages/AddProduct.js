476178b7e0c6fd99a27cc77278aafe7f506829ab
mercklyzer <mercklyzer@gmail.com>
2022-03-21 01:36:36 +0800

    Can now upload multiple images

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,47 +1,73 @@
 import React, { useState } from "react"
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import app from "../firebase";
+import useForm from "../hooks/useForm";
 
 const AddProduct = props => {
-    const [displayImg,setDisplayImg] = useState()
-    const [previewImg,setPreviewImg] = useState()
-    const [otherImgs,setOtherImgs] = useState([])
+    const [product, setProduct, clearProduct] = useForm({
+        displayImg: '',
+        previewImg: '',
+        otherImgs: [],
+        title: '',
+        desc: '',
+        price: 0,
+        category: '',
+        stock: 0
+    })
+
 
     const handleChangeImage = e => {
-        const image = e.target.files[0]
-        console.log(e.target.files);
-        const filename = new Date().getTime() + image.name
+
+        let imagesToUpload = Array.from(e.target.files)
         const storage = getStorage(app)
-        const storageRef = ref(storage, filename)
+        const promises = []
 
-        const uploadTask = uploadBytesResumable(storageRef, image);
+        imagesToUpload.forEach(img => {
+            const filename = new Date().getTime() + img.name
+            const storageRef = ref(storage, filename)
+            const uploadTask = uploadBytesResumable(storageRef, img);
+    
+            const prom = new Promise((fulfill, reject) => {
+                uploadTask.on('state_changed', 
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                        default:
+                    }
+                }, 
+                (error) => {
+                    // Handle unsuccessful uploads
+                    reject(error)
+                }, 
+                async () => {
+                    // Handle successful uploads on complete
+                    const downloadURL = await getDownloadURL(uploadTask.snapshot.ref)
+                    console.log('File available at', downloadURL);
+                        
+                    fulfill(downloadURL)
+                });
+            })
 
-        uploadTask.on('state_changed', 
-        (snapshot) => {
+            promises.push(prom)
+        })
 
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-            case 'paused':
-                console.log('Upload is paused');
-                break;
-            case 'running':
-                console.log('Upload is running');
-                break;
-            default:
+        Promise.all(promises)
+        .then(images => {
+            let newImage = {
+                target: {
+                    name:e.target.name,
+                    value: e.target.name === 'otherImgs'? images : images[0]
+                }
             }
-        }, 
-        (error) => {
-            // Handle unsuccessful uploads
-        }, 
-        () => {
-            // Handle successful uploads on complete
-            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            console.log('File available at', downloadURL);
-            setImage(downloadURL)
-            });
-        });
+            setProduct(newImage)
+        })
     }
 
     return(
@@ -50,20 +76,26 @@ const AddProduct = props => {
 
             <div className="mt-4">
                 <div className="text-gray-700 font-semibold mb-2">Display Image</div>
-                <input type="file" name="displayImg" onChange={(e) => handleChangeImage(e)} />
-                {image && <img src={image} className=""/>}
+                <input 
+                    type="file" 
+                    name="displayImg"
+                    onChange={(e) => handleChangeImage(e)} 
+                />
+                {product.displayImg && <img src={product.displayImg} className=""/>}
             </div>
 
             <div className="mt-4">
                 <div className="text-gray-700 font-semibold mb-2">Preview Image</div>
                 <input type="file" name="previewImg" onChange={(e) => handleChangeImage(e)} />
-                {image && <img src={image} className=""/>}
+                {product.previewImg && <img src={product.previewImg} className=""/>}
             </div>
 
             <div className="mt-4">
                 <div className="text-gray-700 font-semibold mb-2">Other Images</div>
-                <input type="file" name="otherImgs" onChange={(e) => handleChangeImage(e)} />
-                {image && <img src={image} className=""/>}
+                <input type="file" name="otherImgs" onChange={(e) => handleChangeImage(e)} multiple/>
+                {
+                    product.otherImgs.map((img, i) => <img src={img} key={i}/>)
+                }
             </div>
 
             <div className="mt-4">
@@ -87,7 +119,7 @@ const AddProduct = props => {
                     <option className="border px-2 py-1 rounded-sm w-96" value="bed">Bed</option>
                 </select>
             </div>
-            
+            1
             <div className="mt-4">
                 <div className="text-gray-700 font-semibold mb-2">Stock</div>
                 <input className="border px-2 py-1 rounded-sm w-96" type="number" defaultValue={1} min={1}/>

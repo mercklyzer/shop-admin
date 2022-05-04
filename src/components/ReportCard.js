a782dd0a7025f281c2231ed17dd277969c2052d6
mercklyzer <mercklyzer@gmail.com>
2022-05-04 21:06:36 +0800

    adjust arrow on stats

--- src/components/ReportCard.js
+++ src/components/ReportCard.js
@@ -14,8 +14,8 @@ const ReportCard = ({title, figure, percentage, currency, isLoading}) => {
             <div className="mb-4 flex items-center">
                 <div className="mr-8 font-bold text-4xl">{currency? '$':''}{<CountUp end={figure} duration={2}/>}</div>
                 <div className="mr-4 font-medium text-lg">{percentage !== "Inf"? `${(percentage*100).toFixed(1)}%` : ''}</div>
-                {percentage !== "Inf" && percentage > 0 && <svg className="w-6 h-6 stroke-red-600" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>}
-                {percentage !== "Inf" && percentage < 0 && <svg className="w-6 h-6 rotate-180 stroke-green-600" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>}
+                {percentage !== "Inf" && percentage < 0 && <svg className="w-6 h-6 stroke-red-600" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>}
+                {percentage !== "Inf" && percentage > 0 && <svg className="w-6 h-6 rotate-180 stroke-green-600" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>}
             </div>
             {<div className={`mb-4 font-semibold text-gray-500 ${percentage === "Inf"? 'hidden':''}`}>Compared to last month</div>}
             
